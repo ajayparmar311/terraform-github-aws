c9134886afa3f703e561c920c35dd46cf0de9a10
ajayparmar311 <103115368+ajayparmar311@users.noreply.github.com>
2025-07-12 16:24:11 +0530

    Update backend.tf

--- terraform-code/backend.tf
+++ terraform-code/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "irada-terraform-state"  # Must match manually created bucket
     key            = "global/s3/production/terraform.tfstate"  # Path to store state
-    region         = "us-east-1"              # Same as bucket region
+    region         = "us-east-2"              # Same as bucket region
     dynamodb_table = "terraform-locks"        # Must match DynamoDB table name
     encrypt        = true                     # Enables server-side encryption
   }
