d8dd75631d56ff0b544b5d66dff21140298c1736
ajayparmar311 <ajayp.parmar311@gmail.com>
2025-07-14 21:32:22 +0000

    adding admin user ajayparmar

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -20,7 +20,24 @@ resource "aws_iam_user" "terraform-test" {
   }
 }
 
+resource "aws_iam_user" "admin_user" {
+  name = "ajayparmar"
+  tags = {
+    Purpose = "Admin user created by Terraform"
+  }
+}
+
+resource "aws_iam_user_policy_attachment" "admin_policy" {
+  user       = aws_iam_user.admin_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+
+
+
 resource "aws_iam_user_policy_attachment" "attach_s3_readonly" {
   user       = aws_iam_user.terraform-test.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
+
+
